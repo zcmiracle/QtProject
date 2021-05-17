7758481352503117a8595d42b685ec369b458ba9
Fearless <489368600@qq.com>
2021-05-17 12:17:23 +0800

    信号与槽函数补充

--- 02_signal_and_slot/mainwindow.cpp
+++ 02_signal_and_slot/mainwindow.cpp
@@ -4,6 +4,15 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    /**
+       信号Signal 与 槽Slot
+       信号（Signal）：比如点击按钮就会发出一个点击信号
+       槽（Slot）：一般也叫做槽函数，是用来处理信号的函数
+
+       一个信号可以被多个槽同时处理，可以定义空信号和空槽
+
+     */
+
     QPushButton *button = new QPushButton;
     button->setText("登录");
     button->setFixedSize(100, 30);
