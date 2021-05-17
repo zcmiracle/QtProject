8afe6c3a23e7f1210d3cf79a4174f0e78f904768
zcmiracle <489368600@qq.com>
2021-05-17 21:01:45 +0800

    ui图中转为槽并且可以修改函数名

---
+++ 04_signal_and_slot_ui/mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>u>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+// 点击ui中的按钮，右键选择【转为槽】，自动生成此函数
+void MainWindow::on_pushButton_clicked()
+{
+    qDebug() << "on_pushButton_clicked()";
+}
+
+void MainWindow::on_registerButton_clicked()
+{
+    qDebug() << "on_registerButton_clicked()";
+}
--- QtProject.pro
+++ QtProject.pro
@@ -3,4 +3,5 @@ TEMPLATE = subdirs
 SUBDIRS += \
     01-mac_ffmpeg \
     02_signal_and_slot \
-    03_custom_signal_and_slot
+    03_custom_signal_and_slot \
+    04_signal_and_slot_ui
